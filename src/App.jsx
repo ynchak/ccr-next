f4fc7a63ce66161387c689bae2a2fb36d5150e93
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2024-02-21 15:28:34 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import { NextUIProvider } from '@nextui-org/react';
+import { ThemeProvider } from 'src/components/theme-provider';
 
 import { Layout } from './components/layout';
 import { Home } from './pages/home-page';
@@ -11,14 +12,16 @@ function App() {
   const navigate = useNavigate();
   return (
     <NextUIProvider navigate={navigate}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/checking" element={<Checking />} />
-          <Route path="/etc" element={<Etc />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/checking" element={<Checking />} />
+            <Route path="/etc" element={<Etc />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
     </NextUIProvider>
   );
 }
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,14 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Header from './header';
-import { ThemeProvider } from './theme-provider';
 
 export function Layout() {
   return (
     <>
-      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <Header />
-        <Outlet />
-      </ThemeProvider>
+      <Header />
+      <Outlet />
     </>
   );
 }
