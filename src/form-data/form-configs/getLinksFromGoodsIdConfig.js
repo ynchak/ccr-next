f5419b55fd251c6723986f9b10e33217faedac8b
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2024-02-29 15:43:42 +0200

    count lines

--- src/form-data/form-configs/getLinksFromGoodsIdConfig.js
+++ src/form-data/form-configs/getLinksFromGoodsIdConfig.js
@@ -1,10 +1,11 @@
 import { addToClipboard } from 'src/utils';
 
-const action = (column) => {
+const action = (column, _, setCountLines) => {
   const links = column
     .split('\n')
     .filter((line) => line !== '')
     .map((id) => `https://rozetka.com.ua/ua/${id}/p${id}/`);
+  setCountLines(links.length);
   addToClipboard(links.join('\n'));
 };
 export const getLinksFromGoodsIdConfig = {
